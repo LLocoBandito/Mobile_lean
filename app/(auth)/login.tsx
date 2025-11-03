8fb80e14067005ca2dab52e3061722edfb7e99a5
SatyaAnanda <indrasatyayt112@gmail.com>
2025-11-03 22:35:10 +0800

    login-senggol

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -36,7 +36,7 @@ const LoginScreen: FC = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Selamat Datang lagi</Text>
+      <Text style={styles.title}>Selamat Datang lagi senggol</Text>
 
       {errorMessage && (
         <View style={styles.errorContainer}>
